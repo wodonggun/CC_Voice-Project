763ec3a47b068b5c3f285b795d4989214ddd331e
Woo, Dong Keun <35188271+wodonggun@users.noreply.github.com>
2019-05-16 16:45:42 +0900

    Rename audio_cutting.py to audio_cut_Intersection.py

---
+++ audio_cut_Intersection.py
@@ -0,0 +1,46 @@
+import wave
+import warnings
+warnings.filterwarnings('ignore',category=RuntimeWarning) #ffmpeg error control
+import pyaudio
+
+
+CHANNELS = 1    # 1:MONO  2: STEREO 
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+RATE = 44100
+
+RECORD_SECONDS = 10   # sampling time
+
+
+# recording setting
+WAVE_OUTPUT_FILENAME = "test.wav"
+p = pyaudio.PyAudio()
+stream = p.open(format=FORMAT, 
+                channels=CHANNELS, 
+                rate=RATE, 
+                input=True, 
+                frames_per_buffer=CHUNK)
+
+
+# recording start
+try:
+    print("* recording........") 
+    frames = []
+    for i in range(0, int(RATE/CHUNK     * RECORD_SECONDS)): 
+        data = stream.read(CHUNK) 
+        frames.append(data)
+
+finally:                
+    print("* done recording")
+    stream.stop_stream() 
+    stream.close() 
+    p.terminate() 
+
+
+# wav_file write
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb') 
+wf.setnchannels(CHANNELS) 
+wf.setsampwidth(p.get_sample_size(FORMAT)) 
+wf.setframerate(RATE) 
+wf.writeframes(b''.join(frames)) 
+wf.close()
