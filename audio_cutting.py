576921e5572f778165922948bf286c1ee8d974c8
Woo, Dong Keun <35188271+wodonggun@users.noreply.github.com>
2019-05-16 16:59:06 +0900

    Update audio_cutting.py

--- audio_cutting.py
+++ audio_cutting.py
@@ -1,3 +1,5 @@
+import warnings
+warnings.filterwarnings('ignore',category=RuntimeWarning) #ffmpeg error control
 from pydub import AudioSegment
 
 
@@ -8,27 +10,29 @@ def audio_cutting(time_stamp, cutting_range):
     # Slice audio
     # pydub는 milliseconds 단위를 사용한다
 
-    
-    first_10_seconds = song[(time_stamp-cutting_range)*1000 : (time_stamp+cutting_range)*1000]
+    #cutting 범위 언더플로우 방지 
+    start_time = max(time_stamp-cutting_range,0)
+    #cutting 범위 오버플로우 방지
+    end_time =   min(time_stamp+cutting_range,len(song) / (1000))
+
+
+    my_audio = song[(start_time)*1000 : (end_time)*1000]
 
 
 
     # up/down volumn    
-    beginning = first_10_seconds + 10
+    beginning = my_audio + 10
 
-    print("hello")
     # Save the result
     # can give parameters-quality, channel, etc
     beginning.export('result.wav', format='wav', parameters=["-q:a", "10", "-ac", "1"])
 
 
-# 몇초지점에서 자를건지
+
+
 time_stamp = 5
 cutting_range = 2
 
 audio_cutting(time_stamp,cutting_range)
 
 print("오디오 커팅 종료")
-
-
-# Reference (https://github.com/jiaaro/pydub)
